fbe289ad24af4cdc7f81fd7f307c207eb0a5b591
riaz37 <riaz37.ipe@gmail.com>
2025-08-22 06:08:05 +0600

    try

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
@@ -14,4 +13,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig; 
